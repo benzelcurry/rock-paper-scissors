589c651d91a4c19ff8bc49938aa3ecbc50cbbe32
Ben <benawhiting@gmail.com>
2022-06-07 18:28:06 -0700

    Create computerPlay() fx and begin playRound() fx

---
+++ script.js
@@ -0,0 +1,37 @@
+/* Pseudocode for project:
+    1. Create function computerPlay to randomly return rock, paper, or scissors
+    2. Create function playRound() to process the results
+        2.1. Take two parameters: playerSelection, computerSelection
+        2.2. Return string that declares winner
+            2.2.1. ex. "You lose! Paper beats Rock"
+        2.3. Make function case-insensitive
+        2.4. Make function return the results
+    3. Create function called game()
+        3.1. Call playRound() function
+        3.2. Use for loop to make game last five rounds
+        3.3. Use console.log() to display results of each round and final winner
+        3.4. Use prompt() to get input from user */
+
+function computerPlay() {
+    const choices = ["Rock", "Paper", "Scissors"];
+
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];
+}
+
+function playRound(playerSelection, computerSelection) {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            return "Draw! Both users selected rock."
+        }
+        else if (computerSelection == "paper") {
+            return "You lose! Paper beats rock."
+        }
+        else {
+            return "You win! Rock beats scissors."
+        }
+    }
+}
